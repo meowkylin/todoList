2793cfd345d23c27190aa334875d331a12d3cf51
Kevin Lin <meowkylin@gmail.com>
2016-04-22 17:03:37 +0800

    create app & api

--- api.js
+++ api.js
@@ -2,21 +2,11 @@
 var express = require('express')
 var app		= express()
 var db     	= require('./db')
-var engine  = require('ejs-locals')
 var bodyParser = require('body-parser')
 var mongoose= require('mongoose')
-var routes	= require('./routes/index4app')
-
-app.engine('ejs',require('ejs').renderFile)
-app.set('view engine','ejs')
-app.set('views',__dirname + '/views')
+var routes	= require('./routes/index')
 app.use(bodyParser.json())
 app.use( bodyParser.urlencoded({ extended : true }))
-
-app.use((req,res,next) => {
-	console.log(req.method + ' ' + req.url)
-	next()
-})
 
 app.get('/api/',routes.index)
 app.post('/api/create',routes.create)
